ab8567323cd2a3dd8651d0bb08ff21649918c24c
LeeGunHyeong <khyeong1002@gmail.com>
2022-12-08 02:00:52 +0900

    Heap 정보 추가(#25)

--- src/data/accessibility.js
+++ src/data/accessibility.js
@@ -6,16 +6,29 @@ export const accessibilityCollection = {
 				{
 					name: "자료구조",
 					description: {
-						text: "자료구조",
+						text: "자료구조 관련",
 						links: [
 							["자료구조 개념과 종류", "https://bnzn2426.tistory.com/115"],
 						]
-					}
+					},
+					skills: [
+						{
+							name: "Heap",
+							description: {
+								text: "Heap 관련",
+								links: [
+									["Heap의 정의", "https://gmlwjd9405.github.io/2018/05/10/data-structure-heap.html"],
+									["자료구조 Heap과 Heap 영역은 같은가?", "https://www.inflearn.com/questions/163538/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%ED%9E%99%EA%B3%BC-%EB%A9%94%EB%AA%A8%EB%A6%AC-%ED%9E%99"],
+								]
+							}
+						},
+					]
 				},
+				
 				{
 					name: "알고리즘 개념과 종류",
 					description: {
-						text: "알고리즘",
+						text: "알고리즘 관련",
 						links: [
 							["알고리즘 개념과 종류", "https://okiidokii.tistory.com/190"],
 						]
@@ -24,7 +37,7 @@ export const accessibilityCollection = {
 				{
 					name: "자료구조&알고리즘",
 					description: {
-						text: "Hash",
+						text: "Hash 관련",
 						links: [
 							["Hash란?", "https://go-coding.tistory.com/30"],
 							["Hash 탐색법", "https://excelsior-cjh.tistory.com/125"]
